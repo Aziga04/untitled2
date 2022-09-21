e18920ac80108db9bd5a125d4a5b26a396037235
Aziga04 <azizmusaev33@gmail.com>
2022-09-21 10:20:36 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Boss bossOne = new Boss();
+        bossOne.setHealth(500);
+        bossOne.setDamage(50);
+        bossOne.setWeapon(new Weapon(WeaponType.GUM, "Desert eagle"));
+        System.out.println(bossOne.info());
+
+        Skeleton skeletonOne = new Skeleton();
+        skeletonOne.setHealth(200);
+        skeletonOne.setDamage(35);
+        skeletonOne.setWeapon(new Weapon(WeaponType.BOW,"BowMaster Rebel"));
+        skeletonOne.setArrows(15);
+        System.out.println(skeletonOne.info());
+
+        Skeleton skeletonTwo = new Skeleton();
+        skeletonTwo.setHealth(200);
+        skeletonTwo.setDamage(50);
+        skeletonTwo.setWeapon(new Weapon(WeaponType.BOW,"Yarrow Gamma"));
+        skeletonTwo.setArrows(15);
+        System.out.println(skeletonTwo.info());
+    }
+}
---
+++ src/WeaponType.java
@@ -0,0 +1,3 @@
+public enum WeaponType {
+        AKM,GUM, BOW;
+}
